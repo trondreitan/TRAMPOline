f448a1181a0f16986892ebdee5215b1d7c674006
Trond Reitan <trond.reitan.1969@gmail.com>
2021-09-03 22:11:12 +0200

    Add files via upload
    Comparison of abundance-focused and main manuscript model when it comes to relative species abundance.

--- compare_rel_abundance.R
+++ compare_rel_abundance.R
@@ -1,6 +1,6 @@
 source("read_data.R")
 
-models=c("f_sf","lambda_f_sf","f_sf_ou","f_sf_o18bin","f_sf_cabin")
+models=c("f_sf","lambda_f_sf")
 n.mod=length(models)
 
 ra.mean=array(NA,c(n.mod,Nf,N.sp))
@@ -24,116 +24,73 @@ for(i in 1:n.mod)
   ra.upper[i,,]=upper.rel.abundance	
 }
 
-sp=names(y)[22:25]
+# Re-done
 
-
-cols=c("black","purple","red","green","blue")
-
-png("rel_abundance_compare2.png",height=1200,width=1600)
-par(cex=3,cex.lab=1.9,cex.sub=1.9,cex.axis=1.9,mar=c(10,5,1,1))
-par(mfrow=c(2,2))
-plot(K, ra.mean[1,,1], type="b", ylim=c(min(ra.mean[,,1]),max(ra.mean[,,1])),
-  xlab="time", ylab=sprintf("Relative abundance: %s",sp[1]),lwd=4,log="y")
-lines(K, ra.mean[2,,1],lty=2,col=cols[2],lwd=3)
-lines(K, ra.mean[3,,1],lty=3,col=cols[3],lwd=3)
-lines(K, ra.mean[4,,1],lty=4,col=cols[4],lwd=3)
-lines(K, ra.mean[5,,1],lty=5,col=cols[5],lwd=3)
-
-plot(K, ra.mean[1,,2], type="b", ylim=c(min(ra.mean[,,2]),max(ra.mean[,,2])),
-  xlab="time", ylab=sprintf("Relative abundance: %s",sp[2]),lwd=4,log="y")
-lines(K, ra.mean[2,,2],lty=2,col=cols[2],lwd=3)
-lines(K, ra.mean[3,,2],lty=3,col=cols[3],lwd=3)
-lines(K, ra.mean[4,,2],lty=4,col=cols[4],lwd=3)
-lines(K, ra.mean[5,,2],lty=5,col=cols[5],lwd=3)
-
-plot(K, ra.mean[1,,3], type="b", ylim=c(min(ra.mean[,,3]),max(ra.mean[,,3])),
-  xlab="time", ylab=sprintf("Relative abundance: %s",sp[3]),lwd=4,log="y")
-lines(K, ra.mean[2,,3],lty=2,col=cols[2],lwd=3)
-lines(K, ra.mean[3,,3],lty=3,col=cols[3],lwd=3)
-lines(K, ra.mean[4,,3],lty=4,col=cols[4],lwd=3)
-lines(K, ra.mean[5,,3],lty=5,col=cols[5],lwd=3)
-
-plot(K, ra.mean[1,,4], type="b", ylim=c(min(ra.mean[,,4]),max(ra.mean[,,4])),
-  xlab="time", ylab=sprintf("Relative abundance: %s","Superspecies"),lwd=4,log="y")
-lines(K, ra.mean[2,,4],lty=2,col=cols[2],lwd=3)
-lines(K, ra.mean[3,,4],lty=3,col=cols[3],lwd=3)
-lines(K, ra.mean[4,,4],lty=4,col=cols[4],lwd=3)
-lines(K, ra.mean[5,,4],lty=5,col=cols[5],lwd=3)
-dev.off()
-
-
-
+F=FALSE
+cols=c("black","darkgrey")
 
 png("rel_abundance_compare_uncert.png",height=1200,width=1600)
-par(cex=3,cex.lab=1.9,cex.sub=1.9,cex.axis=1.9,mar=c(10,5,1,1))
+par(mar=c(5,10,5,5), oma=c(7,4,0,0))
+par(cex=3,cex.lab=3,cex.sub=3,cex.axis=3,cex.main=3)
 par(mfrow=c(2,2))
-plot(K, ra.mean[1,,1], type="b", ylim=c(min(ra.lower[,,1]),max(ra.upper[,,1])),
-  xlab="time", ylab=sprintf("Relative abundance: %s",sp[1]),lwd=4,log="y")
-lines(K, ra.mean[2,,1],lty=1,col=cols[2],lwd=3)
-#lines(K, ra.mean[3,,1],lty=1,col=cols[3],lwd=3)
-#lines(K, ra.mean[4,,1],lty=1,col=cols[4],lwd=3)
-#lines(K, ra.mean[5,,1],lty=1,col=cols[5],lwd=3)
+plot(K, ra.mean[1,,1], type="b",    
+   ylim=c(0.001,1), xlab="", ylab="",
+   main=substitute(paste(italic('A. tongima'))),   
+   axes=F, pch=20, cex=2.5, col=cols[1],
+   lwd=6,log="y")
+axis(1, at=seq(-2, 0, 0.5), labels=NA)
+axis(2, at=c(0.001, 0.01, 0.1, 1), labels=c(0.001,0.01, 0.1, 1))
+box()
+lines(K, ra.mean[2,,1],lty=1,col=cols[2],lwd=6)
 lines(K, ra.lower[1,,1],lty=2,col=cols[1])
 lines(K, ra.lower[2,,1],lty=2,col=cols[2])
-#lines(K, ra.lower[3,,1],lty=2,col=cols[3])
-#lines(K, ra.lower[4,,1],lty=2,col=cols[4])
-#lines(K, ra.lower[5,,1],lty=2,col=cols[5])
 lines(K, ra.upper[1,,1],lty=2,col=cols[1])
 lines(K, ra.upper[2,,1],lty=2,col=cols[2])
-#lines(K, ra.upper[3,,1],lty=2,col=cols[3])
-#lines(K, ra.upper[4,,1],lty=2,col=cols[4])
-#lines(K, ra.upper[5,,1],lty=2,col=cols[5])
 
-plot(K, ra.mean[1,,2], type="b", ylim=c(min(ra.lower[,,2]),max(ra.upper[,,2])),
-  xlab="time", ylab=sprintf("Relative abundance: %s",sp[2]),lwd=4,log="y")
-lines(K, ra.mean[2,,2],lty=1,col=cols[2],lwd=3)
-#lines(K, ra.mean[3,,2],lty=1,col=cols[3],lwd=3)
-#lines(K, ra.mean[4,,2],lty=1,col=cols[4],lwd=3)
-#lines(K, ra.mean[5,,2],lty=1,col=cols[5],lwd=3)
+plot(K, ra.mean[1,,2], type="b",   
+   ylim=c(1e-14,1),xlab="", ylab="",
+   main=substitute(paste(italic('E. excavata'))),
+   axes=F, pch=20, cex=2.5, col=cols[1],
+   lwd=6,log="y")
+axis(1, at=seq(-2, 0, 0.5), labels=NA)
+axis(2, at=c(1e-14, 1e-9, 0.0001, 1), labels=c(1e-14, 1e-9, 0.0001, 1))
+box()
+lines(K, ra.mean[2,,2],lty=1,col=cols[2],lwd=6)
 lines(K, ra.lower[1,,2],lty=2,col=cols[1])
 lines(K, ra.lower[2,,2],lty=2,col=cols[2])
-#lines(K, ra.lower[3,,2],lty=2,col=cols[3])
-#lines(K, ra.lower[4,,2],lty=2,col=cols[4])
-#lines(K, ra.lower[5,,2],lty=2,col=cols[5])
 lines(K, ra.upper[1,,2],lty=2,col=cols[1])
 lines(K, ra.upper[2,,2],lty=2,col=cols[2])
-#lines(K, ra.upper[3,,2],lty=2,col=cols[3])
-#lines(K, ra.upper[4,,2],lty=2,col=cols[4])
-#lines(K, ra.upper[5,,2],lty=2,col=cols[5])
 
-plot(K, ra.mean[1,,3], type="b", ylim=c(min(ra.lower[,,3]),max(ra.upper[,,3])),
-  xlab="time", ylab=sprintf("Relative abundance: %s",sp[3]),lwd=4,log="y")
-lines(K, ra.mean[2,,3],lty=1,col=cols[2],lwd=3)
-#lines(K, ra.mean[3,,3],lty=1,col=cols[3],lwd=3)
-#lines(K, ra.mean[4,,3],lty=1,col=cols[4],lwd=3)
-#lines(K, ra.mean[5,,3],lty=1,col=cols[5],lwd=3)
+plot(K, ra.mean[1,,3], type="b",    
+   ylim=c(1e-10,1),xlab="", ylab="",
+   main=substitute(paste(italic('A. unicornis'))), 
+   axes=F, pch=20, cex=2.5, col=cols[1],
+   lwd=6,log="y")
+axis(1, at=seq(-2, 0, 0.5), labels=seq(2, 0, -0.5),padj=1)
+axis(2, at=c(1e-10, 1e-6, 0.01, 1), labels=c(1e-10, 1e-6,  0.01, 1))
+box()
+lines(K, ra.mean[2,,3],lty=1,col=cols[2],lwd=6)
 lines(K, ra.lower[1,,3],lty=2,col=cols[1])
 lines(K, ra.lower[2,,3],lty=2,col=cols[2])
-#lines(K, ra.lower[3,,3],lty=2,col=cols[3])
-#lines(K, ra.lower[4,,3],lty=2,col=cols[4])
-#lines(K, ra.lower[5,,3],lty=2,col=cols[5])
 lines(K, ra.upper[1,,3],lty=2,col=cols[1])
 lines(K, ra.upper[2,,3],lty=2,col=cols[2])
-#lines(K, ra.upper[3,,3],lty=2,col=cols[3])
-#lines(K, ra.upper[4,,3],lty=2,col=cols[4])
-#lines(K, ra.upper[5,,3],lty=2,col=cols[5])
 
-plot(K, ra.mean[1,,4], type="b", ylim=c(min(ra.lower[,,4]),max(ra.upper[,,4])),
-  xlab="time", ylab=sprintf("Relative abundance: %s","Superspecies"),lwd=4,log="y")
-lines(K, ra.mean[2,,4],lty=1,col=cols[2],lwd=3)
-#lines(K, ra.mean[3,,4],lty=1,col=cols[3],lwd=3)
-#lines(K, ra.mean[4,,4],lty=1,col=cols[4],lwd=3)
-#lines(K, ra.mean[5,,4],lty=1,col=cols[5],lwd=3)
+plot(K, ra.mean[1,,4], type="b",    
+   ylim=c(0.6,1),xlab="", ylab="",
+   main="Superspecies",
+   axes=F, pch=20, cex=2.5, col=cols[1],
+   lwd=6,log="y")
+axis(1, at=seq(-2, 0, 0.5), labels=seq(2, 0, -0.5),padj=1)
+axis(2, at=seq(0.6,1.0,0.1), labels=seq(0.6,1.0, 0.1))
+box()
+lines(K, ra.mean[2,,4],lty=1,col=cols[2],lwd=6)
 lines(K, ra.lower[1,,4],lty=2,col=cols[1])
 lines(K, ra.lower[2,,4],lty=2,col=cols[2])
-#lines(K, ra.lower[3,,4],lty=2,col=cols[3])
-#lines(K, ra.lower[4,,4],lty=2,col=cols[4])
-#lines(K, ra.lower[5,,4],lty=2,col=cols[5])
 lines(K, ra.upper[1,,4],lty=2,col=cols[1])
 lines(K, ra.upper[2,,4],lty=2,col=cols[2])
-#lines(K, ra.upper[3,,4],lty=2,col=cols[3])
-#lines(K, ra.upper[4,,4],lty=2,col=cols[4])
-#lines(K, ra.upper[5,,4],lty=2,col=cols[5])
+mtext(side=1, "millions of years ago", outer=T, cex=3.8, padj=1)
+mtext(side=2, "Relative species abundance, R\n", outer=T, cex=3.8, padj=1)
 dev.off()
+
 
 
